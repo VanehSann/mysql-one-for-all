5034cb18005ca4446c4c1f9f8db6602b572cc8bd
VanehSann <vanessacbsantos22@gmail.com>
2022-06-03 20:46:05 -0300

    adicionando o subscription_date e comentários

--- desafio1.sql
+++ desafio1.sql
@@ -2,6 +2,8 @@ DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE SpotifyClone;
 
+-- criando tabela de planos
+-- plan_id, plan_price e plan_name
 CREATE TABLE SpotifyClone.plans(
     plan_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     -- decimal(3,2) vi na thread da Regislane 19-B
@@ -9,10 +11,13 @@ CREATE TABLE SpotifyClone.plans(
     plan_name CHAR(20) NOT NULL
 ) engine = InnoDB;
 
+-- criando tabela de users
+-- user_id, user_age, user_name, plan_id e subscription_date
 CREATE TABLE SpotifyClone.users(
     user_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     user_name CHAR(20) NOT NULL,
     user_age INT NOT NULL,
+    subscription_date CHAR(20) NOT NULL,
     plan_id INT NOT NULL,
     FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
 ) engine = InnoDB;
@@ -25,15 +30,15 @@ VALUES
   ('universitário', 5.99),
   ('pessoal', 6.99);
 
-INSERT INTO SpotifyClone.users (user_name, user_age, plan_id)
+INSERT INTO SpotifyClone.users (user_name, user_age, plan_id, subscription_date)
 VALUES
- ('Thati',	23, 1),
- ('Cintia',	35, 2),
- ('Bill',	20, 3),
- ('Roger',	45, 4),
- ('Norman',	58, 4),
- ('Patrick',	33, 2),
- ('Vivian',	26, 3),
- ('Carol',	19, 3),
- ('Angelina',	42, 2),
- ('Paul',	46, 2);
+ ('Thati',	23, 1, '2019-10-20'),
+ ('Cintia',	35, 2, '2017-12-30'),
+ ('Bill',	20, 3, '2019-06-05'),
+ ('Roger',	45, 4, '2020-05-13'),
+ ('Norman',	58, 4, '2017-02-17'),
+ ('Patrick',	33, 2, '2017-01-06'),
+ ('Vivian',	26, 3, '2018-01-05'),
+ ('Carol',	19, 3, '2018-02-14'),
+ ('Angelina',	42, 2, '2018-04-29'),
+ ('Paul',	46, 2, '2017-01-17');
