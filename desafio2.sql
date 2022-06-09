aca039147a46ed072da3d5fe622eabc21e08327e
Vanessa Santos <53117898+VanehSann@users.noreply.github.com>
2022-06-08 21:17:32 -0300

    alternativa mais assertiva pro desafio02.sql

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,17 @@
-SELECT
-max(b.song_id) as `cancoes`,
-max(a.artist_id) as `artistas`,
-max(a.album_id) as `albuns`
+SELECT 
+count(distinct b.song_id) as `cancoes`,
+count(distinct a.artist_id) as `artistas`,
+count(distinct a.album_id)  as `albuns`
 FROM albums as a
 INNER JOIN songs as b
 ON a.album_id = b.album_id;
+
+-- alternativa
+
+-- SELECT
+-- max(b.song_id) as `cancoes`,
+-- max(a.artist_id) as `artistas`,
+-- max(a.album_id) as `albuns`
+-- FROM albums as a
+-- INNER JOIN songs as b
+-- ON a.album_id = b.album_id;
