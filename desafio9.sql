ff774128f88fb43fc4527c378687f6be4d9d6ccb
Vanessa Santos <53117898+VanehSann@users.noreply.github.com>
2022-06-10 10:22:58 -0300

    Update desafio9.sql

--- desafio9.sql
+++ desafio9.sql
@@ -3,5 +3,4 @@ count(a.user_id) as `quantidade_musicas_no_historico`
 from users as a
 inner join reproductions_history as b
 on b.user_id = a.user_id
-where a.user_name like "Bill"
-;
+where a.user_name like "Bill";
