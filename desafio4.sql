ad9122f01508d6fbe847a41fb906ece52fb4e74e
Vanessa Santos <53117898+VanehSann@users.noreply.github.com>
2022-06-10 04:52:33 -0300

    Update desafio4.sql

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+select distinct b.user_name as `usuario`,
+if(date(max(a.played_date)) like '2021%', 'Usuário Ativo', 'Usuário Inativo') as `condicao_usuario`
+from reproductions_history as a
+inner join users as b
+on a.user_id = b.user_id
+group by b.user_name
+order by usuario asc;
